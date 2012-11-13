b7764a80c9ec542a101f594d9413c8aa68fb1c28
Tatsuya Miyamoto <hondasports@gmail.com>
2012-11-13 16:03:16 +0900

    Added Table View controller

---
+++ ListViewController.h
@@ -0,0 +1,13 @@
+//
+//  ListViewController.h
+//  sample
+//
+//  Created by MIYAMOTO TATSUYA on 2012/11/13.
+//  Copyright (c) 2012年 MIYAMOTO TATSUYA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ListViewController : UITableViewController
+
+@end
